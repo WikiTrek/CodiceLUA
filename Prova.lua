cc233149cea30ce9cfee5fe63d1beae52dfe8181
Luca Mauri <lucamauri@users.noreply.github.com>
2020-02-05 00:34:17 +0100

    Auto commit by PageToGitHub2020-02-05T00:34:17+01:00

--- Prova.lua
+++ Prova.lua
@@ -1,1 +1,36 @@
-UXVlc3RhIMOoIHVuYSBwcm92YQ==
+Upload 2020-02-05
+-- Questo codice proviene dal Modulo:FunzioniGeneriche
+
+local p = {} --p stands for packag
+
+function p.Esempio(frame)
+    local SubPageName
+    local SubPageTitle
+    local Title
+    local pre = mw.html.create('pre')
+    local DoubleLF = string.char(10) .. string.char(10)
+
+    if not frame.args[1] then
+        SubPageName='Esempio'
+    else
+        SubPageName=frame.args[1]
+    end
+
+    Title =  mw.title.getCurrentTitle()
+    SubPageTitle = mw.title.makeTitle(Title.namespace, Title.text .. '/' .. SubPageName)
+
+    local Intro = 'Questo esempio è automaticamente generato tramite script LUA a partire dal codice di esempio presente in <code>[[' .. SubPageTitle.prefixedText .. ']]</code>'
+    local CodeString = 'Il codice'
+    local ReturnString = 'restituisce'
+
+    pre
+       :css( 'width', '65%' )
+       :wikitext(mw.text.nowiki(SubPageTitle:getContent()))
+    return  Intro .. DoubleLF .. CodeString .. DoubleLF .. tostring(pre) .. DoubleLF  .. ReturnString .. DoubleLF .. frame:expandTemplate{ title = SubPageTitle }
+end
+
+function p.test()
+	return "KO"
+end
+
+return p
