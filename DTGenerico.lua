e0cff39f2982833ca1a5f6cdf91572dc2330937d
Luca Mauri <lucamauri@users.noreply.github.com>
2020-03-14 11:31:44 +0100

    Auto commit by PageToGitHub2020-03-14

--- DTGenerico.lua
+++ DTGenerico.lua
@@ -1,3 +1,3 @@
--- Auto upload by PageToGitHub on 2020-03-14T11:27:30+01:00
--- This code from page Modulo:DTGenerico
--- Prova 'github'
+-- [P2G] Auto upload by PageToGitHub on 2020-03-14T11:31:43+01:00
+-- [P2G] This code from page Modulo:DTGenerico
+-- Prova 'github' 2
