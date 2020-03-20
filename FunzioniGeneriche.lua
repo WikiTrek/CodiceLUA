486e179093e64308cb1d3f4a7b53115f1d4b6811
Luca Mauri <lucamauri@users.noreply.github.com>
2020-03-20 19:13:12 +0100

    Aggiunta "TableFromArray"

--- FunzioniGeneriche.lua
+++ FunzioniGeneriche.lua
@@ -1,5 +1,6 @@
--- Auto upload by PageToGitHub on 2020-02-17T22:04:21+01:00
--- This code from page Modulo:FunzioniGeneriche
+-- [P2G] Auto upload by PageToGitHub on 2020-03-20T19:13:11+01:00
+-- [P2G] This code from page Modulo:FunzioniGeneriche
+-- Keyword: wikitrek
 local p = {} --p stands for package
 
 function p.EsempioTemplate(frame)
@@ -27,7 +28,39 @@ function p.EsempioTemplate(frame)
        :wikitext(mw.text.nowiki(SubPageTitle:getContent()))
     return  Intro .. DoubleLF .. CodeString .. DoubleLF .. tostring(pre) .. DoubleLF  .. ReturnString .. DoubleLF .. frame:expandTemplate{ title = SubPageTitle }
 end
-
+function p.TableFromArray(AllRows)
+	local Table = mw.html.create('table')
+	local First
+	local Tr
+	local Cell
+	
+	
+	for _, Row in pairs(AllRows) do
+		Tr = mw.html.create('tr')
+		First = true
+		for _, Cell in pairs(Row) do
+			if First then
+				Cell = mw.html.create('th')
+			else
+				Cell = mw.html.create('td')
+			end
+		end
+	end
+	
+	--[==[if mw.wikibase.getEntity() then
+		Text = "Modifica i dati nella [[:datatrek:Item:" .. mw.wikibase.getEntityIdForCurrentPage() .. "|pagina della entità]] su ''DataTrek''"
+	else
+		Text = "Impossibile trovare l'entità collegata"
+	end]==]
+	
+	p
+	   :css('font-size', 'smaller')
+       :css('text-align', 'right')
+       :css('margin', '1px')
+       :wikitext(Text)
+    return  tostring(p)
+end
+	
 function p.NoWiki(frame)
     return mw.text.nowiki(frame.args[1])
 end
