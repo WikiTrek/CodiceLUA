aebf81ae16224a61327fcb5543cc6729be9e2675
Luca Mauri <lucamauri@users.noreply.github.com>
2020-04-04 16:52:13 +0200

    Auto commit by PageToGitHub2020-04-04

--- DTGenerico.lua
+++ DTGenerico.lua
@@ -1,4 +1,4 @@
--- [P2G] Auto upload by PageToGitHub on 2020-04-04T16:50:11+02:00
+-- [P2G] Auto upload by PageToGitHub on 2020-04-04T16:52:11+02:00
 -- [P2G] This code from page Modulo:DTGenerico
 -- Keyword: wikitrek
 local TableFromArray = require('Modulo:FunzioniGeneriche').TableFromArray
@@ -90,7 +90,7 @@ function p.Incipit(frame)
 	--local SeasonData = p.SeasonInfoRaw()
 	
 	--return frame:expandTemplate{ title = 'DataBoxEpisodio' } .. string.char(10) .. "[[" .. mw.title.getCurrentTitle().text .. "]] è un episodio della stagione " .. SeasonData.SeasonNumber .. " di ''[[" .. SeasonData.SeriesName .. "]]''." .. string.char(10)
-	return "'''{{PAGENAME}}''' è un " .. mw.wikibase.getLabelByLang(p.QFromP('P14'), 'it') .. string.char(10)
+	return "'''" .. frame:expandTemplate{title = 'PAGENAME'} .. "''' è un " .. mw.wikibase.getLabelByLang(p.QFromP('P14'), 'it') .. string.char(10)
 end
 	
 --[==[
