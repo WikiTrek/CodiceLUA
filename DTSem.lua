b32dafffd3dbaf00bd31afaf8498ebfa9351fd38
Luca Mauri <lucamauri@users.noreply.github.com>
2020-05-20 23:27:24 +0200

    Etichetta corretta

--- DTSem.lua
+++ DTSem.lua
@@ -1,4 +1,4 @@
--- [P2G] Auto upload by PageToGitHub on 2020-05-20T23:25:55+02:00
+-- [P2G] Auto upload by PageToGitHub on 2020-05-20T23:27:23+02:00
 -- [P2G] This code from page Modulo:DTSem
 -- Keyword: wikitrek
 local p = {}
@@ -16,44 +16,6 @@ function p.TypeFromDT(frame)
 	end
 	
 	Type = Item['claims']['P49'][1].mainsnak.datavalue.value
-	return "[[Has type::" .. Type .. "|''Date'']]"
-end
-function p.LabelByLang2(frame)
-	local Lang = frame.args['Lingua']
-	local Item = frame.args['Item']
-	if not Lang then
-		Lang = 'it'
-	end
-	
-	if not Item then
-		Item = mw.wikibase.getEntityIdForCurrentPage()
-	end
-	if not Item then
-		Item = 'Q1'
-	end
-	
-	return mw.wikibase.getLabelByLang(Item, Lang)
-end
-function p.FirstSemantic(frame)
-	local query = '[[Category:Episodi]] OR [[Category:Film]][[Character::Spock]]|?Actor'
-	local result = mw.smw.ask(query)
-	--return result
-	
-	local List = {}
-	for _, Row in pairs(result) do
-		local Items = {}
-		for _, Field in pairs(Row) do
-			Items[#Items + 1] = Field
-		end
-		List[#List + 1] = "*" .. table.concat(Items, ', ')
-	end
-	
-	return table.concat(List, string.char(10))
-end
-
-function p.EsempioDataSem(frame)
-	local Date = "2019-03-07"
-	
-	return "[[Prima TV CBS::" .. Date .. "|" .. frame:expandTemplate{title = 'TimeL', args = {Tipo='ITEstesa', Istante=Date}} .. "]]"
+	return "[[Has type::" .. Type .. "|''" .. Type .. "'']]"
 end
 return p
