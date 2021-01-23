db03de9fc2afe769da72270920d2ebe08b397bc1
Luca Mauri <lucamauri@users.noreply.github.com>
2021-01-23 17:40:03 +0100

    Auto commit by PageToGitHub2021-01-23

--- wikitrek-DTGenerico.lua
+++ wikitrek-DTGenerico.lua
@@ -1,4 +1,4 @@
--- [P2G] Auto upload by PageToGitHub on 2021-01-23T17:28:13+01:00
+-- [P2G] Auto upload by PageToGitHub on 2021-01-23T17:40:02+01:00
 -- [P2G] This code from page Modulo:wikitrek-DTGenerico
 -- Keyword: wikitrek
 local TableFromArray = require('Modulo:FunzioniGeneriche').TableFromArray
@@ -182,14 +182,19 @@ function p.ListHTData(frame)
 	if Item.claims['P79'] then
 		local DIV = mw.html.create('div')
 		local DataString
+		local QualiString
 		local HTNodes = Item.claims['P79'][1]
 		
-		DataString = "Informazioni importate dal database di '''HyperTrek''' datato " .. HTNodes.mainsnak.datavalue.value.time
+		DataString = "Informazioni originali lette dal database di '''HyperTrek''' datato " .. HTNodes.mainsnak.datavalue.value.time .. " con i seguenti dettagli: "
+		
+		for _, Qualifier in pairs(HTNodes.qualifiers) do
+			QualiString = QualiString .. "<li>" .. Qualifier.property .. ": </li>" 
+		end
 		
 		DIV
 			:addClass('catlinks')
 			:attr('id', 'htdata')
-			:wikitext(DataString)
+			:wikitext(DataString .. "<ul>" .. QualiString .. "</ul>")
 		return tostring(DIV)
 	else
 		return ""
