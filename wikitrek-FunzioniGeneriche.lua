c249574388c44f3cc1e5a7a672d55cce8055a8b6
Luca Mauri <lucamauri@users.noreply.github.com>
2021-04-07 00:13:26 +0200

    Auto commit by PageToGitHub2021-04-07

--- wikitrek-FunzioniGeneriche.lua
+++ wikitrek-FunzioniGeneriche.lua
@@ -1,4 +1,4 @@
--- [P2G] Auto upload by PageToGitHub on 2021-04-07T00:12:08+02:00
+-- [P2G] Auto upload by PageToGitHub on 2021-04-07T00:13:25+02:00
 -- [P2G] This code from page Modulo:wikitrek-FunzioniGeneriche
 -- Keyword: wikitrek
 local p = {} --p stands for package
@@ -109,7 +109,7 @@ function p.TextColor(frame)
       G = tonumber("0x"..hex:sub(2,2))*17/255
       B = tonumber("0x"..hex:sub(3,3))*17/255
     else
-    	return (hex:sub(1,2))
+    	return (mw.text.nowiki(hex))
       --R = tonumber("0x"..hex:sub(1,2))/255
       --G = tonumber("0x"..hex:sub(3,4))/255
       --B = tonumber("0x"..hex:sub(5,6))/255
