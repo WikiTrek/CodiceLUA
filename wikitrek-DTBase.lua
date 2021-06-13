de250357e4eaf2e2ce0b2a0ca5ab03aa33274703
Luca Mauri <lucamauri@users.noreply.github.com>
2021-06-13 15:29:36 +0200

    Auto commit by PageToGitHub2021-06-13

--- wikitrek-DTBase.lua
+++ wikitrek-DTBase.lua
@@ -1,4 +1,4 @@
--- [P2G] Auto upload by PageToGitHub on 2021-06-13T15:28:52+02:00
+-- [P2G] Auto upload by PageToGitHub on 2021-06-13T15:29:36+02:00
 -- [P2G] This code from page Modulo:wikitrek-DTBase
 --- This module represent the package containing basic functions to access data from the WikiBase instance DataTrek
 -- @module p
@@ -490,7 +490,7 @@ function p.PropertyList(PName, SMWPrefix)
 		Item = 'Q1'
 	end
 	
-	local Statements = mw.wikibase.getAllStatements(Item, PName)
+	local Statements = mw.wikibase.getAllStatements(Item, "P59")
 	if not Statements then
 		return "Nessun riferimento trovato"
 	elseif table.getn(Statements) == 1 then
