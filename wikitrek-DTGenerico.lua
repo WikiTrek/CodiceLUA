7d74a5fefdc2967223ebf306df847e83f5019448
Luca Mauri <lucamauri@users.noreply.github.com>
2021-08-16 18:01:14 +0200

    Auto commit by PageToGitHub2021-08-16

--- wikitrek-DTGenerico.lua
+++ wikitrek-DTGenerico.lua
@@ -1,4 +1,4 @@
--- [P2G] Auto upload by PageToGitHub on 2021-08-16T17:59:37+02:00
+-- [P2G] Auto upload by PageToGitHub on 2021-08-16T18:01:14+02:00
 -- [P2G] This code from page Modulo:wikitrek-DTGenerico
 -- Keyword: wikitrek
 local TableFromArray = require('Modulo:FunzioniGeneriche').TableFromArray
@@ -123,9 +123,6 @@ function p.ListAllP(frame)
 							end
 							--AccValues[#AccValues + 1] = LabelOrLink(SnakValue.qualifiers['P73'][1].datavalue.value['id']) .. " " .. LabelOrLink(Value['id']) .. ", " .. LabelOrLink(SnakValue.qualifiers['P76'][1].datavalue.value['id']) .. ", " .. LabelOrLink(SnakValue.qualifiers['P77'][1].datavalue.value['id'])
 							AccValues[#AccValues + 1] = Assignment
-						elseif Property == "P14" then --CASE Instance
-							AccValues[#AccValues + 1] = AffiliationTree(frame)
-							AccValues[#AccValues + 1] = OperatorTree(frame)
 						elseif Value['entity-type'] == 'item' then
 							local GenericItem
 							if AddSemantic then
@@ -156,7 +153,12 @@ function p.ListAllP(frame)
 							AccValues[#AccValues + 1] = 'TABLE'
 						end
 					else
-						AccValues[#AccValues + 1] = Value
+						if Property == "P14" then --CASE Instance
+							AccValues[#AccValues + 1] = AffiliationTree(frame)
+							AccValues[#AccValues + 1] = OperatorTree(frame)
+						else
+							AccValues[#AccValues + 1] = Value
+						end
 					end
 				end
 				AllRows[#AllRows + 1] = {Header, AccValues}
