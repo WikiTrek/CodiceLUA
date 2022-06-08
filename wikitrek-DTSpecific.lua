7be09294f4f91cf50ae0147a4c4730f759cf8d11
Luca Mauri <lucamauri@users.noreply.github.com>
2022-06-08 22:46:36 +0200

    Auto commit by PageToGitHub2022-06-08

--- wikitrek-DTSpecific.lua
+++ wikitrek-DTSpecific.lua
@@ -1,4 +1,4 @@
--- [P2G] Auto upload by PageToGitHub on 2022-06-08T10:13:09+02:00
+-- [P2G] Auto upload by PageToGitHub on 2022-06-08T22:46:35+02:00
 -- [P2G] This code from page Modulo:wikitrek-DTSpecific
 --- This module represent the package containing specific functions to access data from the WikiBase instance DataTrek
 -- @module p
@@ -256,7 +256,7 @@ function p.SecBoxContent(frame)
 			LI =  mw.html.create('li')
 			
 			if Item == 0 then
-				LI:wikitext("[[Tabella riassuntiva di " .. Short .. "|Tutti]]")
+				LI:wikitext("[[Episodi di " .. Short .. "|Tutti]]")
 			elseif Item == 1 then
 				LI:wikitext("Stagioni: [[Stagione " .. Item .. " di " .. Short .. "|" .. Item .. "]]")
 			else
