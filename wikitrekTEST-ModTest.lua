98e67ee67e7e1e2bbff03444e8719a51ea80f9de
Luca Mauri <lucamauri@users.noreply.github.com>
2023-11-24 23:57:17 +0100

    Test nuova autenticazione

---
+++ wikitrekTEST-ModTest.lua
@@ -0,0 +1,14 @@
+-- [P2G] Auto upload by PageToGitHub on 2023-11-24T23:57:16+01:00
+-- [P2G] This code from page Modulo:wikitrekTEST-ModTest
+--- This module represent the package containing basic functions to access data from the WikiBase instance DataTrek
+-- @module p
+-- @author Luca Mauri [[Utente:Lucamauri]]
+-- Add other authors below
+-- Keyword: wikitrekTEST
+local p = {}
+function p.Epilogo(frame)
+	local DoubleReturn = string.char(10) .. string.char(10) .. string.char(10)
+	
+	return frame:expandTemplate{ title = 'paginecheportanoqui' } .. DoubleReturn .. "== Collegamenti esterni ==" .. DoubleReturn .. p.ExtLinks(frame) .. DoubleReturn .. "==" .. frame:expandTemplate{ title = 'Etichetta', args = {Tipo=Annotazioni} } .. "==" .. string.char(10) ..  mw.text.nowiki("<references/>") .. DoubleReturn .. string.char(10) .. frame:expandTemplate{ title = 'NavGlobale' }  .. "categorie"
+end
+return p
