63a8ad5d76c3a5d473c2500be7293392d77b6a98
Luca Mauri <lucamauri@users.noreply.github.com>
2023-11-30 22:11:09 +0100

    NOT minor

--- wikitrekTEST-ModTest.lua
+++ wikitrekTEST-ModTest.lua
@@ -1,4 +1,4 @@
--- [P2G] Auto upload by PageToGitHub on 2023-11-30T22:05:16+01:00
+-- [P2G] Auto upload by PageToGitHub on 2023-11-30T22:11:08+01:00
 -- [P2G] This code from page Modulo:wikitrekTEST-ModTest
 --- This module represent the package containing basic functions to access data from the WikiBase instance DataTrek
 -- @module p
@@ -7,7 +7,7 @@
 -- Keyword: wikitrekTEST
 local p = {}
 function p.Epilogo(frame)
-	local DoubleReturn = string.char(10) .. string.char(10)
+	local DoubleReturn = string.char(10) .. string.char(10) .. string.char(10) .. string.char(10)
 	
 	return frame:expandTemplate{ title = 'paginecheportanoqui' } .. DoubleReturn .. "== Collegamenti esterni ==" .. DoubleReturn .. p.ExtLinks(frame) .. DoubleReturn .. "==" .. frame:expandTemplate{ title = 'Etichetta', args = {Tipo=Annotazioni} } .. "==" .. string.char(10) ..  mw.text.nowiki("<references/>") .. DoubleReturn .. string.char(10) .. frame:expandTemplate{ title = 'NavGlobale' }  .. "categorie"
 end
